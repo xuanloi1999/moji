4372c8c25981878c05af29992bce86b5d6f5bd68
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2025-11-04 22:25:48 +0700

    add types

---
+++ frontend/src/types/store.ts
@@ -0,0 +1,21 @@
+import type { User } from "@/types/user";
+
+export interface AuthState {
+  accessToken: string | null;
+  user: User | null;
+  loading: boolean;
+
+  setAccessToken: (accessToken: string) => void;
+  clearState: () => void;
+  signUp: (
+    username: string,
+    password: string,
+    email: string,
+    firstName: string,
+    lastName: string
+  ) => Promise<void>;
+  signIn: (username: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
+  fetchMe: () => Promise<void>;
+  refresh: () => Promise<void>;
+}
---
+++ frontend/src/types/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  _id: string;
+  username: string;
+  email: string;
+  displayName: string;
+  avatarUrl?: string;
+  bio?: string;
+  phone?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
